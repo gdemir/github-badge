0cab8da8bbee63b0ebe434dc4cf22efb93a982f9
gökhan demir <gokhan.demir@bil.omu.edu.tr>
2011-02-02 15:41:03 +0200

    minor

--- gb-gist.js
+++ gb-gist.js
@@ -2,70 +2,69 @@ var GitHubbadge = {};
 
 GitHubbadge = new function() {	
   this.init = function() {
-	this.tarayici("js/jquery.js");
-	this.tarayici("js/jquery.template.js", "GitHubbadge.dizin_yukle");
+    this.tarayici("js/jquery.js");
+    this.tarayici("js/jquery.template.js", "GitHubbadge.dizin_yukle");
   }
 
   this.dizin_yukle = function() {
-	document.write('<link rel = "stylesheet" type = "text/css" href = "http://gdemir.me/github-badge/css/gb-gist.css" media = "screen" />');
+    document.write('<link rel = "stylesheet" type = "text/css" href = "http://gdemir.me/github-badge/css/gb-gist.css" media = "screen" />');
     this.badge_yukle();
   }
 
   this.badge_yukle = function() {
-	  document.write("<div id='gb-gist'>");
-	  GitHubbadge.tarayici("http://gist.github.com/api/v1/json/gists/" + GITHUB_USER + "?callback=GitHubbadge.gist");
-	  document.write("</div>");
+    document.write("<div id='gb-gist'>");
+      GitHubbadge.tarayici("http://gist.github.com/api/v1/json/gists/" + GITHUB_USER + "?callback=GitHubbadge.gist");
+    document.write("</div>");
   };
   
   this.gist = function(data) {
-	  (function($){
-		var head   = $("<div>" + GITHUB_TITLE + "(<a href='http://gist.github.com/" + GITHUB_USER + "'>" + GITHUB_USER + "</a>)</div>");
-		var template = $.template(
-		  "<ul>"
-		  +  "<img src='http://gdemir.me/chrome/sembol/git.png' alt='public' title='${description}'>"
-		  +  "<strong><a href='http://gist.github.com/${repo}' title='${description}' target='_blank'>${repo}</a></strong>"
-		  +  "<div id='gb-gist-describe'>"
-			 +  "<li class='gist'>dosya           : ${files}      </li>"
-			 +  "<li class='gist'>oluşturulma     : ${created_at} </li>"
-			 //~ +  "<li class='gist'>genel durum     : ${public}     </li>"
-			 //~ +  "<li class='gist'>tanım           : ${description}</li>"
-			 //~ +  "<li class='gist'>sahip           : ${owner}      </li>"
-		  +  "</div>"
-	   +  "</ul>"
-		);
-		var footer = $(
-		   "<div id='gb-gist-footer'>"
-			 + '<p></p>'
-			 + '<span>designed | by gdemir</span>'
-		 + "</div>"
-		 );
-		//head ekle
-		$('#gb-gist').append(head);
-		//repo ekle
-		repos = data.gists.sort();
-		$.each(repos, function(index) {
-								$('#gb-gist').append(template, this);
-					  });
-		var goster = $("<br/><div><a href='#' class='goster'>" + GITHUB_SHOW + " (" + repos.length + ")</a></div>")
-		.click(function(event) {
-					$('#gb-gist  ul').show();
-					$('#gb-gist .goster').hide();
-					return false;
-			   });
-		$('#gb-gist ul')
-		.click(function(event) {
-					$(event.currentTarget).find('.gist').toggle();
-			   })
+    (function($){
+      var head   = $("<div>" + GITHUB_TITLE + "(<a href='http://gist.github.com/" + GITHUB_USER + "'>" + GITHUB_USER + "</a>)</div>");
+      var template = $.template(
+        "<ul>"
+        +  "<img src='http://gdemir.me/chrome/sembol/git.png' alt='public' title='${description}'>"
+        +  "<strong><a href='http://gist.github.com/${repo}' title='${description}' target='_blank'>${repo}</a></strong>"
+        +  "<div id='gb-gist-describe'>"
+          +  "<li class='gist'>dosya           : ${files}      </li>"
+          +  "<li class='gist'>oluşturulma     : ${created_at} </li>"
+      //~ +  "<li class='gist'>genel durum     : ${public}     </li>"
+      //~ +  "<li class='gist'>tanım           : ${description}</li>"
+      //~ +  "<li class='gist'>sahip           : ${owner}      </li>"
+        +  "</div>"
+     +  "</ul>");
+      var footer = $(
+	  "<div id='gb-gist-footer'>"
+	    + '<p></p>'
+	    + '<span>designed | by gdemir</span>'
+	+ "</div>");
+     //head ekle
+     $('#gb-gist').append(head);
+     //repo ekle
+     repos = data.gists.sort();
+     $.each(repos, function(index) {
+		    $('#gb-gist').append(template, this);
+	            });
+     var goster = $("<br/><div><a href='#' class='goster'>" + GITHUB_SHOW + " (" + repos.length + ")</a></div>")
+     .click(function(event) {
+	      $('#gb-gist  ul').show();
+	      $('#gb-gist .goster').hide();
+	      return false;
+	   });
+     $('#gb-gist ul')
+     .click(function(event) {
+	      $(event.currentTarget).find('.gist').toggle();
+	    })
 	    .find('.gist')
-		.hide()
+	    .hide()
 	    .end()
-		.filter(':gt(' + (GITHUB_LENGTH - 1) + ')').hide();
-		if ($('#gb-gist ul').is(':hidden'))
-			$('#gb-gist ').append(goster);
-		//footer ekle
-		$('#gb-gist').append(footer);
-	  })(jQuery);
+	    .filter(':gt(' + (GITHUB_LENGTH - 1) + ')').hide();
+     if ($('#gb-gist ul').is(':hidden'))
+       $('#gb-gist ').append(goster);
+     //footer ekle
+    $('#gb-gist').append(footer);
+    })(jQuery);
   };
+
   this.tarayici = function (url, callback) {
     if ("jQuery" in window && url.match(/^http/)) {
       if (typeof callback != "undefined")
