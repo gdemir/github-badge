9726ffb646064034c8d2d8c5c31a9570feb6e0f7
gdemir <gdemir@bil.omu.edu.tr>
2011-09-15 22:42:27 +0300

    .

---
+++ gb-gist.js
@@ -0,0 +1,68 @@
+GitHubBadge.gist = function(data) {
+  (function($){
+    var template = $.template(
+      "<li class='public clickable'>"
+      +  "<img src='http://gdemir.me/github-badge/images/gist.png' alt='public' title='${description}'>"
+      +  "<strong><a href='${url}' title='${description}' target='_blank'>${files}</a></strong>"
+      +  "<div class='description'>${files}</div>"
+      +"</li>"
+    );
+
+    var list = $("<div class='repos'><ul id='repo_listing'></ul></div>");
+    $('#github-badge .body')
+      .empty()
+      .append(list);
+    list = list.find('ul');
+    orderedRepos = data.gists;
+    $.each(orderedRepos, function(index) {
+      list.append(template, this);
+    });
+    var showLimit = window.GITHUB_LIST_LENGTH || 10;
+
+    var showAllName = ("GITHUB_SHOW_ALL" in window && GITHUB_SHOW_ALL) || 'Show all';
+    var showMore = $("<div><a href='#' class='more'>" + showAllName + " (" + orderedRepos.length + ")</a></div>")
+      .find('a')
+      .click(function(event) {
+        $('#github-badge .body li').show();
+        $('#github-badge .more').hide();
+        return false;
+      });
+
+    $('#github-badge .body li')
+    .click(function(event) {
+      $(event.currentTarget).find('.description').toggle();
+    })
+    .find('.description')
+      .hide()
+      .end()
+    .filter(':gt(' + (showLimit - 1) + ')').hide() // hide extras
+    if ($('#github-badge .body li').is(':hidden'))
+      $('#github-badge .body').append(showMore);
+
+  })(jQuery);
+};
+
+(function($){
+  $.fn.buildBody = function() {
+    return this.append($("<div class='body'>loading...</div>"));
+  };
+
+  $.fn.buildHeader = function(title, username) {
+    var template = $.template(
+      "<div class='header'><span class='title'>${title}</span> <span>("
+      +   "<a href='http://github.com/${username}'>${username}</a>)"
+      + "</span></div>")
+    return this.append(template, { title: title, username: username });
+  };
+
+  $.fn.buildFooter = function() {
+    return this.append($(
+      "<div class='footer'>"
+        + "<a href='http://gdemir.me/github-badge'>GitHub Badge</a>"
+        + " | "
+        + "Written by <a href='http://gdemir.me'>Gökhan Demir</a>"
+      + "</div>"
+      + "</fieldset>"
+      ));
+  };
+})(jQuery);
---
+++ github-badge-gist.js
@@ -0,0 +1,59 @@
+var GitHubBadge = {};
+
+GitHubBadge = new function() {
+  this.init = function() {
+
+    this.tarayici("javascripts/jquery.js");
+    this.tarayici("gb-gist.js");
+    this.tarayici("javascripts/jquery.template.js", "GitHubBadge.css_yukle");
+  }
+
+  this.css_yukle = function() {
+    if(typeof jQuery == 'undefined' || typeof jQuery.template == 'undefined')
+      throw("GitHub Badge requires jQuery and jQuery.template");
+
+    var url = 'stylesheets/github-badge.css';
+    var style_id = 'badge';
+    if ("jQuery" in window) {
+        jQuery('head').prepend(
+            jQuery('<link rel="stylesheet" type="text/css" href="' + url + '" id="' + style_id + '"></link>')
+        );
+    } else {
+      document.write('<link rel="stylesheet" href="'+url+'" type="text/css"></link>');
+    }
+    this.user_yukle(GITHUB_USERNAME);
+  }
+
+  this.tarayici = function( url, callback ) {
+    if ("jQuery" in window && url.match(/^http/)) {
+      if (typeof callback != "undefined") {
+        jQuery.getScript(url,function() { eval(callback + "()") });
+      } else {
+        jQuery.getScript(url);
+      }
+    } else {
+      onLoadStr = (typeof callback == "undefined") ? "" : 'onload="' + callback + '()" ';
+      document.write('<script ' + onLoadStr + 'type="text/javascript" src="' + url + '"></script>');
+    }
+  }
+
+  this.user_yukle = function(username) {
+    (function($){
+      var title = ("GITHUB_TITLE" in window && GITHUB_TITLE) || 'My projects';
+      $('#github-badge')
+        .empty()
+        .buildHeader(title, username)
+        .buildBody(username)
+        .buildFooter();
+    })(jQuery);
+    this.badge_yukle(username);
+  };
+
+
+  this.badge_yukle = function(username) {
+      this.tarayici(
+        "http://gist.github.com/api/v1/json/gists/" + username + "?callback=GitHubBadge.gist");
+  };
+};
+
+GitHubBadge.init();
